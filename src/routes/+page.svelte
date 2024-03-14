7ef7f02c76171fe006ad503316ed4222718ee6ca
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-03-14 22:10:40 +0000

    Calculate progress and feed it into progress bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import { ProgressBar } from '@skeletonlabs/skeleton';
+	import { differenceInCalendarDays } from 'date-fns';
+
+	const startDate: Date = new Date(2024, 2, 13);
+	const endDate: Date = new Date(2024, 3, 11)
+
+	const daysLeftToGo = differenceInCalendarDays(endDate, Date.now())
+	const maxProgress = differenceInCalendarDays(endDate, startDate) + 1
+	const progress = maxProgress - daysLeftToGo
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
 		<h2 class="h2">Trak Zak</h2>
 
-		<ProgressBar label="Progress Bar" value={80} max={100} />
+		<ProgressBar label="Progress Bar" min={0} value={progress} max={maxProgress} />
 
 	</div>
 </div>
