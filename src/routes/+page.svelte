221cb226c207200c6bd9f8961f76e7daaad8bbe1
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-03-18 18:07:46 +0000

    Add gif of cartoon Zak on his journey

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { ProgressBar } from '@skeletonlabs/skeleton';
 	import { differenceInCalendarDays } from 'date-fns';
+	import zakRunning from '$lib/assets/zak-running.gif'
 
 	const startDate: Date = new Date(2024, 2, 13);
 	const endDate: Date = new Date(2024, 3, 11)
@@ -13,6 +14,7 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center w-full m-5 flex flex-col items-center">
 		<h2 class="h2">Trak Zak</h2>
+		<img src="{zakRunning}" alt="Animation of a man walking" width="10%" height="10%" />
 		<ProgressBar label="Progress Bar" min={0} value={progress} max={maxProgress} />
 		<h3>{daysLeftToGo} Days to go</h3>
 	</div>
