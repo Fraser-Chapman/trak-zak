be00cc6249d9bc64f62f855269962826e44443ad
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-03-19 22:34:24 +0000

    Add animation to running man causing him to slide in from the left and run along the progress bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 		<h2 class="h2">Trak Zak</h2>
 
 		<div class="flex flex-row justify-between">
-			<img style="margin-left: {((progress / maxProgress) * 100) - 6}%" src="{zakRunning}" alt="Animation of a man walking" width="15%" height="15%" />
+			<img class="slide-in" style="margin-left: {((progress / maxProgress) * 100) - 6}%" src="{zakRunning}" alt="Animation of a man walking" width="15%" height="15%" />
 			<img src="{coastalCliffs}" alt="" width="15%" height="15%" />
 		</div>
 
@@ -25,3 +25,17 @@
 		<h3>{daysLeftToGo} Days to go</h3>
 	</div>
 </div>
+
+<style>
+    .slide-in {
+        position: relative;
+        left: -100%; /* Off-screen initially */
+        animation: slideIn 2s forwards; /* Animation duration and direction */
+    }
+
+    @keyframes slideIn {
+        to {
+            left: 0; /* Slide to the left edge */
+        }
+    }
+</style>
